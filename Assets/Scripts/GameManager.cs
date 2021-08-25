c20328071f0e692302bf629cd15829b4be60902c
CRSylar <cristianoromaldetti@icloud.com>
2021-08-25 11:04:43 +0200

    ongoing

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+	public List<GameObject> targets;
+
+	private float spawnRate = 1.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine( SpawnTarget() );
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	IEnumerator SpawnTarget()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds( spawnRate );
+
+			int index = Random.Range( 0, targets.Count );
+			Instantiate( targets[index] );
+		}
+	}
+}
